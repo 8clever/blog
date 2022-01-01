a866a6807a6f1f0e2f004d11f42bd9b599b7d685
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-01 21:21:34 +0200

    complete work with home page but we need change mongdb to postgresql because mongodb latest versions not supported by typeorm

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,50 @@
 import { Grid } from '@mui/material'
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { DataBase } from '../server/connectors'
+import { PostEntity } from '../server/entities/Post'
 import { data } from '../src/components/data'
 import FeaturedPost from '../src/components/FeaturedPost'
 import { Layout } from '../src/components/Layout'
 import Main from '../src/components/Main'
 import MainFeaturedPost from '../src/components/MainFeaturedPost'
 import Sidebar from '../src/components/Sidebar'
+import { Blog } from '../src/components/types'
 
-const Home: NextPage = () => {
+interface PageProps {
+  mainPost: Blog.Post,
+  featuredPosts: Blog.Post[],
+  posts: Blog.Post[]
+}
+
+export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
+
+  const db = new DataBase();
+  await db.init();
+  const postsRepo = db.getRepo<PostEntity>(PostEntity.COLLECTION_NAME);
+  const posts = await postsRepo.find({
+    
+  });
+
+  return {
+    props: {
+      mainPost: posts[0],
+      featuredPosts: posts.slice(1,3),
+      posts: posts.slice(3,5)
+    }
+  }
+}
+
+const Home: NextPage<PageProps> = (props) => {
   return (
     <Layout>
-      <MainFeaturedPost post={data.mainFeaturedPost} />
+      <MainFeaturedPost post={props.mainPost} />
       <Grid container spacing={4}>
-        {data.featuredPosts.map((post, idx) => (
+        {props.featuredPosts.map((post, idx) => (
           <FeaturedPost key={idx} post={post} />
         ))}
       </Grid>
       <Grid container spacing={5} sx={{ mt: 3 }}>
-        <Main title="From the firehose" posts={data.posts} />
+        <Main title="From the firehose" posts={props.posts.map(p => p.post)} />
         <Sidebar
           title={data.sidebar.title}
           description={data.sidebar.description}
--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,107 +1,8 @@
 import { GitHub as GitHubIcon, Facebook as FacebookIcon, Twitter as TwitterIcon } from "@mui/icons-material"
-import { Blog } from "./types";
-
-const post1 = `# Sample blog post
-
-#### April 1, 2020 by [Olivier](/)
-
-This blog post shows a few different types of content that are supported and styled with
-Material styles. Basic typography, images, and code are all supported.
-You can extend these by modifying 'Markdown.js'.
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.
-
-Curabitur blandit tempus porttitor. **Nullam quis risus eget urna mollis** ornare vel eu leo.
-Nullam id dolor id nibh ultricies vehicula ut id elit.
-
-Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum.
-Aenean lacinia bibendum nulla sed consectetur.
-
-## Heading
-
-Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-
-### Sub-heading
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-### Sub-heading
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod.
-Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo
-sit amet risus.
-
-- Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-- Donec id elit non mi porta gravida at eget metus.
-- Nulla vitae elit libero, a pharetra augue.
-
-Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
-
-1.  Vestibulum id ligula porta felis euismod semper.
-2.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-3.  Maecenas sed diam eget risus varius blandit sit amet non magna.
-
-Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.`
 
 const sections = [
-  { title: 'Technology', url: '#' },
-  { title: 'Design', url: '#' },
-  { title: 'Culture', url: '#' },
-  { title: 'Business', url: '#' },
-  { title: 'Politics', url: '#' },
-  { title: 'Opinion', url: '#' },
-  { title: 'Science', url: '#' },
-  { title: 'Health', url: '#' },
-  { title: 'Style', url: '#' },
-  { title: 'Travel', url: '#' },
+  { title: 'Home', url: '/' },
 ];
-
-const mainFeaturedPost: Blog.Post = {
-  key: "main-featured-post",
-  title: 'Title of a longer featured blog post',
-  post: "",
-  description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: {
-    url: "https://source.unsplash.com/random",
-    label: "main image description"
-  },
-  dateCreated: new Date().toJSON()
-};
-
-const featuredPosts: Blog.Post[] = [
-  {
-    key: "some_featured_post",
-    post: "",
-    title: 'Featured post',
-    dateCreated: new Date().toJSON(),
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: {
-      url: "https://source.unsplash.com/random",
-      label: "Image Text"
-    }
-  },
-  {
-    key: "another_featured_post",
-    post: "",
-    title: 'Post title',
-    dateCreated: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: {
-      url: "https://source.unsplash.com/random",
-      label: "Image Text"
-    }
-  },
-];
-
-const posts = [post1, post1, post1];
 
 const sidebar = {
   title: 'About',
@@ -116,8 +17,5 @@ const sidebar = {
 
 export const data = {
   sidebar,
-  posts,
-  featuredPosts,
-  mainFeaturedPost,
   sections
 }
