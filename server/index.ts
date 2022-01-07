a006f365e83cc424d7bf412e66d3a0155e63bd62
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-08 01:44:36 +0200

    add small fix

--- server/index.ts
+++ server/index.ts
@@ -2,13 +2,19 @@ import { createServer } from "http"
 import next from "next"
 import { parse } from "url"
 import { rootConfig } from "../rootConfig"
+import { DataBase } from "./connectors"
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler();
 
 async function bootstrap () {
-  await app.prepare();
+  const db = new DataBase();
+
+  await Promise.all([
+    db.init(),
+    app.prepare()
+  ]);
 
   const server = createServer((req, res) => {
     const parsedUrl = parse(req.url || "", true)
