6b32ab2d2360ef6fde07b916f4e16c25d9dc142f
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-08 01:20:02 +0200

    add small fix

--- server/middlewares/secure.ts
+++ server/middlewares/secure.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest } from "next"
 import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
+import { rootConfig } from "../../rootConfig";
 
 /** middlewares can access env only with direct access pattern */
 export const secureMiddleware = async (req: NextApiRequest) => {
-  const session = await getToken({
+  const config = {
     req,
-    secret: process.env.SECRET as string,
-    secureCookie: process.env.NEXTAUTH_URL?.startsWith("https://")
-  });
-  console.log(process.env.SECRET);
-  console.log(process.env.NEXTAUTH_URL);
+    secret: process.env.SECRET as string || rootConfig.SECRET,
+    secureCookie: (process.env.NEXTAUTH_URL || rootConfig.NEXTAUTH_URL)?.startsWith("https://")
+  }
+  const session = await getToken(config);
   if (!session) return NextResponse.redirect("/signin")
 }
