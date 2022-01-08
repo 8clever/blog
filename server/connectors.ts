c677bcaa094684b8f106d6789cab62d0e176bdbb
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-08 23:11:30 +0200

    fix db connection

--- server/connectors.ts
+++ server/connectors.ts
@@ -2,6 +2,11 @@ import { MikroORM, EntityName, EntityManager, RequestContext } from "@mikro-orm/
 import { rootConfig } from "../rootConfig";
 import { Image } from "./entities/Image";
 import { Post } from "./entities/Post";
+
+function getGlobal (): { __ORM__?: MikroORM } {
+  return global as any
+}
+
 class DataBase {
 
   em!: EntityManager;
@@ -9,13 +14,13 @@ class DataBase {
   init = async () => {
     if (this.em) return;
 
-    const em = RequestContext.getEntityManager();
-    if (em) {
-      this.em = em;
+    const g = getGlobal();
+    if (g.__ORM__) {
+      this.em = g.__ORM__.em;
       return;
     }
 
-    const orm = await MikroORM.init({
+    g.__ORM__ = await MikroORM.init({
       validate: true,
       ensureIndexes: true,
       type: "mongo",
@@ -27,7 +32,7 @@ class DataBase {
       ]
     });
 
-    this.em = orm.em;
+    this.em = g.__ORM__.em;
   }
 
   getRepo<Entity> (target: EntityName<Entity>) {
