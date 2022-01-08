3e09a65fdfbe299d6603228683cf88cf808ac845
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-08 22:26:52 +0200

    add some visual fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 import { wrap } from '@mikro-orm/core'
-import { Grid } from '@mui/material'
+import { Stack } from '@mui/material'
 import type { GetServerSideProps, NextPage } from 'next'
 import { DataBase, Post } from '../server/connectors'
-import { data } from '../src/components/data'
 import FeaturedPost from '../src/components/FeaturedPost'
 import { Layout } from '../src/components/Layout'
-import Main from '../src/components/Main'
 import MainFeaturedPost from '../src/components/MainFeaturedPost'
-import Sidebar from '../src/components/Sidebar'
 import { Blog } from '../src/components/types'
 
 interface PageProps {
   mainPost?: Blog.Post,
   featuredPosts: Blog.Post[],
-  posts: Blog.Post[]
 }
 
 export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
@@ -22,19 +18,18 @@ export const getServerSideProps: GetServerSideProps<PageProps> = async () => {
   await db.init();
   const postsRepo = db.getRepo<Post>(Post);
   
-  const posts = (await postsRepo.find({}, {
+  const [ mainPost, ...featuredPosts ] = (await postsRepo.find({}, {
     orderBy: {
       dateUpdated: "ASC_NULLS_LAST",
       dateCreated: "ASC"
     },
-    limit: 5
+    limit: 10
   })).map(p => wrap(p).toJSON() as Post);
 
   return {
     props: {
-      mainPost: posts[0] || null,
-      featuredPosts: posts.slice(1,3) || [],
-      posts: posts.slice(3,5) || []
+      mainPost,
+      featuredPosts,
     }
   }
 }
@@ -46,12 +41,11 @@ const Home: NextPage<PageProps> = (props) => {
         props.mainPost &&
         <MainFeaturedPost post={props.mainPost} withLink />
       }
-      <Grid container spacing={3} sx={{ mb: 3 }}>
+      <Stack spacing={3} sx={{ mb: 3 }}>
         {props.featuredPosts.map((post, idx) => (
           <FeaturedPost key={idx} post={post} />
         ))}
-      </Grid>
-      <Main title="From the firehose" posts={props.posts.map(p => p.post)} />
+      </Stack>
     </Layout>
   )
 }
--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Grid, Card, CardActionArea, CardContent, CardMedia } from "@mui/material"
+import { Typography, Card, CardActionArea, CardContent, CardMedia } from "@mui/material"
 import { Blog } from './types';
 
 interface FeaturedPostProps {
@@ -10,31 +10,29 @@ export default function FeaturedPost(props: FeaturedPostProps) {
   const { post } = props;
 
   return (
-    <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href={Blog.Post.GetPostUrl(post)}>
-        <Card sx={{ display: 'flex' }}>
-          <CardContent sx={{ flex: 1 }}>
-            <Typography component="h2" variant="h5">
-              {post.title}
-            </Typography>
-            <Typography variant="subtitle1" color="text.secondary">
-              {post.dateCreated && new Date(post.dateCreated).toLocaleDateString()}
-            </Typography>
-            <Typography variant="subtitle1" paragraph>
-              {post.description}
-            </Typography>
-            <Typography variant="subtitle1" color="primary">
-              Continue reading...
-            </Typography>
-          </CardContent>
-          <CardMedia
-            component="img"
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-            image={post.image.url}
-            alt={post.image.label}
-          />
-        </Card>
-      </CardActionArea>
-    </Grid>
+    <CardActionArea component="a" href={Blog.Post.GetPostUrl(post)}>
+      <Card sx={{ display: 'flex' }}>
+        <CardMedia
+          sx={{ maxWidth: 160 }}
+          component="img"
+          image={post.image.url}
+          alt={post.image.label}
+        />
+        <CardContent sx={{ flex: 1 }}>
+          <Typography component="h2" variant="h5">
+            {post.title}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary">
+            {post.dateCreated && new Date(post.dateCreated).toLocaleDateString()}
+          </Typography>
+          <Typography variant="subtitle1" paragraph>
+            {post.description}
+          </Typography>
+          <Typography variant="subtitle1" color="primary">
+            Continue reading...
+          </Typography>
+        </CardContent>
+      </Card>
+    </CardActionArea>
   );
 }
