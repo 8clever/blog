5642e3d24b8bddb9cea42ac2e7c79d3a320fa866
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-15 21:29:29 +0200

    add small fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,15 +8,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
         </body>
       </Html>
     )
