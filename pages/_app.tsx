1d0f5e428aebbad4fff5aa8fb549a3f889042793
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-17 22:17:53 +0200

    add ad

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "@mui/system";
 import { theme } from 'src/components/theme';
 import { CssBaseline } from '@mui/material';
-import { LazyHydrate } from 'src/components/LazyHydrate';
+import { lazy, LazyHydrate } from 'src/components/LazyHydrate';
+
+lazy(() => {
+  const script = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7579927697787840" crossorigin="anonymous"></script>`;
+  if (typeof document === "undefined") return;
+  document.head.insertAdjacentHTML("beforeend", script);
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
