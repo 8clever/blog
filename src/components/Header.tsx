cd83188dbfae0b8c0a5c7c62fa81037431785df2
Ivan Vityaev <ivan.vitiaev@gost-group.com>
2022-01-18 16:46:55 +0200

    add menu for admin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,21 @@
 import * as React from 'react';
-import { Toolbar, Button, Typography, Link, AppBar, Box, Container } from "@mui/material"
+import { Toolbar, Button, Typography, Link, AppBar, Box, Container, Avatar, Menu, MenuItem, ListItemIcon, ListItemText } from "@mui/material"
 import { signIn, signOut, useSession } from "next-auth/react";
 import { WebSite } from './types';
+import { Add, Login, Logout } from '@mui/icons-material';
 interface HeaderProps {
 }
-
-interface RootSection {
-  title: string;
-  url: string;
-}
-
-const rootSections: RootSection[] = [];
 
 export default function Header(props: HeaderProps) {
   const { status } = useSession();
 
-  const sections = React.useMemo(() => {
-    if (status === "authenticated") {
-      return [
-        ...rootSections,
-        {
-          title: "Admin",
-          url: "/admin"
-        }
-      ]
-    }
-    return rootSections;
-  }, [ status ]);
+  const [ menuAnchor, setMenuAnchor ] = React.useState<HTMLElement | null>(null);
 
   return (
     <Box sx={{ mb: 5 }}>
       <AppBar position='relative' color='transparent'>
         <Container maxWidth="lg">
-          <Toolbar>
+          <Toolbar disableGutters>
             <Typography
               component="h2"
               variant="h5"
@@ -46,50 +29,63 @@ export default function Header(props: HeaderProps) {
             </Typography>
             {
               status === "authenticated" ?
+              <>
+                <Button
+                  onClick={e => setMenuAnchor(e.currentTarget)}
+                  color='primary'
+                  startIcon={
+                    <Avatar 
+                      sx={{
+                        bgcolor: "primary.main",
+                        width: 25,
+                        height: 25
+                      }}
+                      alt="current user" 
+                    />
+                  }>
+                  Admin
+                </Button>
+                <Menu 
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  anchorEl={menuAnchor}
+                  onClose={() => setMenuAnchor(null)}
+                  open={Boolean(menuAnchor)}>
+                  <MenuItem component="a" href="/admin/edit-post">
+                    <ListItemIcon>
+                      <Add />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Post
+                    </ListItemText>
+                  </MenuItem>
+                  <MenuItem onClick={() => signOut()}>
+                    <ListItemIcon>
+                      <Logout />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Sign-out
+                    </ListItemText>
+                  </MenuItem>
+                </Menu>
+              </> :
               <Button 
-                onClick={() => {
-                  signOut();
-                }}
-                variant="outlined" 
-                size="small">
-                Sign out
-              </Button> :
-              <Button 
+                startIcon={<Login />}
                 onClick={() => {
                   signIn();
-                }}
-                variant="outlined" 
-                size="small">
+                }}>
                 Sign in
               </Button>
             }
           </Toolbar>
         </Container>
       </AppBar>
-      {
-        sections.length ?
-        <Container maxWidth="lg">
-          <Toolbar
-            component="nav"
-            variant="dense"
-            sx={{ overflowX: 'auto' }}
-          >
-            {sections.map((section) => (
-              <Link
-                color="inherit"
-                noWrap
-                key={section.title}
-                variant="body2"
-                href={section.url}
-                sx={{ p: 1, flexShrink: 0 }}
-              >
-                {section.title}
-              </Link>
-            ))}
-          </Toolbar>
-        </Container>
-        : null
-      }
     </Box>
   );
 }
